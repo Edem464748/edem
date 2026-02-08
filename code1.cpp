af5ac27407e262309f5843190e84d21196c3227c
Edem464748 <edem.4.00@mail.ru>
2026-02-08 13:00:06 +0300

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <clocale>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    int number;
+    int shift;
+
+    cout << "Введите целое число: ";
+    cin >> number;
+
+    cout << "Введите количество позиций для сдвига: ";
+    cin >> shift;
+
+    int leftShift = number << shift;   
+    int rightShift = number >> shift;  
+
+    cout << "Арифметический сдвиг влево: " << leftShift << endl;
+    cout << "Арифметический сдвиг вправо: " << rightShift << endl;
+
+    return 0;
+}
